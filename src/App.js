4191bc113370748e34b12220479d028c8e01a96d
HarivanshL <112008912+HarivanshL@users.noreply.github.com>
2024-03-05 10:04:21 -0600

    Router setup complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*Insert NavBar*/}
+      <Router>
+        <Route path = '/login'>
+          {/*Add login component*/}
+        </Route>
+        <Route path = '/home'>
+          {/*Add home component*/}
+        </Route>
+      </Router>
     </div>
   );
 }
