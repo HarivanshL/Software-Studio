0b2b073a454534f49d29e32482c5212822f883fc
HarivanshL <112008912+HarivanshL@users.noreply.github.com>
2024-03-05 13:03:33 -0600

    Added comments

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       {/*Insert NavBar*/}
+      {/*Insert more paths as needed*/}
       <Router>
         <Route path = '/login'>
           {/*Add login component*/}
