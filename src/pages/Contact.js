e1a4018ac43455f19ed99f6b92507c01d5eecfea
HarivanshL <112008912+HarivanshL@users.noreply.github.com>
2024-03-08 18:56:27 -0600

    Completed basic contact page info. Requires links to other pages, navbar, linkbar, and images

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,8 +9,12 @@ function Contact() {
             <Navbar />
             <div className='container1'>
                 <h1>Contact us</h1>
-                <p>We are happy to address and concerns or thoughts you would like to share with us</p>
+                <p>We are happy to address any concerns or thoughts you would like to share with us.</p>
+                <p>
+                    Feel free to check out our FAQ page. {/**Insert FAQ hyperlink or Linbar component */}
+                </p>
             </div>
+
             <Form />
 
         </>
