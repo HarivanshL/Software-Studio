443884d0f3228df8d41a95b33502b9178a0cd5d5
HarivanshL <112008912+HarivanshL@users.noreply.github.com>
2024-03-08 19:37:44 -0600

    fixed container on contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,13 +7,13 @@ function Contact() {
     return (
         <>
             <Navbar />
-            <div className='container1'>
+            <Container className='container1'>
                 <h1>Contact us</h1>
                 <p>We are happy to address any concerns or thoughts you would like to share with us.</p>
                 <p>
                     Feel free to check out our FAQ page. {/**Insert FAQ hyperlink or Linbar component */}
                 </p>
-            </div>
+            </Container>
 
             <Form />
 
