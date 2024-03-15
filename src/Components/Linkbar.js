f277758a551a64181935bf668888c02f0345ee68
HarivanshL <112008912+HarivanshL@users.noreply.github.com>
2024-03-15 14:39:28 -0500

    fixed some styling components on contact page

--- src/Components/Linkbar.js
+++ src/Components/Linkbar.js
@@ -3,7 +3,7 @@ import '../css-styling/Linkbar.css'
 import { NavLink } from 'react-router-dom'
 const Linkbar = ({links}) =>{
   return (
-    <div className="buttons row text-center">
+    <div className="buttons row text-center mt-5">
     {links.map((link, index) => (
         <div className="col-md-4" key={index}>
           <NavLink className="button btn-block fixed-width" to={link.route}>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,18 +8,18 @@ function Contact() {
   const links = [{ name: "FAQ", route: "/" }];
   return (
     <>
-      <CustomNavbar />
-      <Container fluid className="container1">
-        <h1 className="mt-3 text-center">Contact us</h1>
+      <CustomNavbar/>
+      <Container fluid className="container1" class = "mt-5" >
+        <h1 className="mt-5 text-center">Contact us</h1>
         <div>
-          <p className="mb-1">
+          <p className="mt-5 mb-5">
             We are happy to address any concerns or thoughts you would like to
             share with us.
           </p>
           <p className="mb-3">Feel free to check out our FAQ page.</p>
         </div>
       </Container>
-      <Linkbar links={links} />
+      <Linkbar class = "mt-5" links={links} />
       <ContactForm />
     </>
   );
